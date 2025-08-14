ffdcdd955bdb854957824e89ecf1a72ce438b8e1
Joe Marini <cjmarini68@gmail.com>
2025-08-14 16:24:30 +0000

    sort start state

--- Start/02_data/sorting_start.py
+++ Start/02_data/sorting_start.py
@@ -1,6 +1,7 @@
 # using the built-in sorted function to sort data content
 
 test_scores = [82, 67, 94, 73, 79, 86, 58, 91, 89, 71, 88, 77, 84]
+test_names = ["Bob", "Doug", "Jane", "Sue", "Ted", "Craig", "Melanie"]
 
 # TODO: perform a simple sort using sorted()
 
